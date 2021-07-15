23fc060a3aa893f10d0936d5d7b351552613eeb7
Melvin Carvalho <melvincarvalho@gmail.com>
2021-07-15 13:20:36 +0200

    first release

---
+++ bin/deployall.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+TMPFILE="/tmp/gitmark.$$.txt"
+DEFAULT_MESSAGE="webcredits"
+MESSAGE="${1:-$DEFAULT_MESSAGE}"
+# set up btm as remote exe
+BTMEXE="ssh ubuntu@157.90.144.229"
+
+
+git pull origin gh-pages
+git add .
+git commit -m "$MESSAGE"
+
+git log -1 --pretty=%s | grep '^gitmark '
+if [ $? -eq 0 ]
+then
+  echo git marked already
+  exit
+fi
+
+git push origin gh-pages
+
+# run twice in case new tx is not there
+git mark
+sleep 1
+git mark > "${TMPFILE}"
+# TODO check for empty tx
+
+cat "${TMPFILE}"
+
+TX=$( tail -1 < "${TMPFILE}" )
+
+echo
+echo "${TX}"
+
+HASH=$(${BTMEXE} "${TX}")
+echo "${HASH}" | grep '^[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f].*'
+if [ $? -eq 1 ]
+then
+  echo no hash found, something went wrong
+  exit
+fi
+RES=$(git commit --allow-empty -m "gitmark ${HASH}")
+echo "${RES}"
+
+git push origin gh-pages
+
